d0ebc81c7d7e697e9e864a310de505e6305beae3
Mücahid Aydin <mucahidaydin047@gmail.com>
2024-06-19 00:57:32 +0300

    Add database-first example

---
+++ DatabaseFirst/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using DatabaseFirst.Entities;
+
+
+Order order1 =new Order() { Test = 1 };
+Console.WriteLine($"order1.Test: {order1.Test}");
+order1.Test = 2;
+Console.WriteLine($"order1.Test: {order1.Test}");
