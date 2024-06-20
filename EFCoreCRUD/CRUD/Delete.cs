9e9191943b04fbeeac38041cbb28589eeea62dcf
Mücahid Aydin <mucahidaydin047@gmail.com>
2024-06-20 23:16:44 +0300

    Add delete functionality for CRUD operations

---
+++ EFCoreCRUD/CRUD/Delete.cs
@@ -0,0 +1,24 @@
+﻿using EFCoreCRUD.Contexts;
+using EFCoreCRUD.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreCRUD.CRUD
+{
+    public class Delete
+    {
+        private int Id { get; set; }
+        public Delete(int Id)
+        {
+            this.Id = Id;
+        }
+
+        public void DeleteData()
+        {
+            ETicaretContext context = new ETicaretContext();
+            Urun? urun = context.Urunler.FirstOrDefault(u => u.Id == Id);
+
+            context.Urunler.Remove(urun);
+            context.SaveChanges();
+        }
+    }
+}
--- EFCoreCRUD/Program.cs
+++ EFCoreCRUD/Program.cs
@@ -13,3 +13,12 @@ Thread.Sleep(10000);
 Update update1 = new Update(Id: urunId, Ad: "Kalem", Fiyat: 15, Stok: 150);
 Urun urunUpdate = update1.UpdateData().Result;
 Console.WriteLine($"Güncellenen ürün: {urunUpdate.Id} - {urunUpdate.Ad} - {urunUpdate.Fiyat} - {urunUpdate.Stok}");
+
+//Sleep 10sn    
+Thread.Sleep(10000);
+
+//Delete
+Delete delete1 = new Delete(Id: urunId);
+delete1.DeleteData();
+Console.WriteLine($"Silinen ürün Id: {urunId}");
+
