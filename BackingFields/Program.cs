88819c869c1a8b48933b22a6f0ef2a4c7a6b1a5f
Mücahid Aydin <mucahidaydin047@gmail.com>
2024-07-13 13:24:40 +0300

    Add backing field examples

---
+++ BackingFields/Program.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine();
+
+class Person1
+{
+    public int Id { get; set; }
+
+    public string name;
+
+    //expression-bodied properties
+    public string Name { get => name.Substring(0, 3); set => name = value.Substring(0, 3); }
+
+    public string Department { get; set; }
+
+}
+
+//Data Annotations
+class Person2
+{
+    public int Id { get; set; }
+
+    public string name;
+    [BackingField(nameof(name))]
+
+    public string Name { get; set; }
+
+    public string Department { get; set; }
+
+}
+
+//Fluent API
+class Person3
+{
+    public int Id { get; set; }
+    public string name;
+    public string Name { get; set; }
+    public string Department { get; set; }
+}
+
+//Field-Only Properties
+class Person4
+{
+    public int Id { get; set; }
+    private string _name;
+    public string Department { get; set; }
+
+    public string GetName()
+    {
+        return this._name;
+    }
+    public void SetName(string value)
+    {
+        this._name = value;
+    }
+}
+class BackingFieldDbContext : DbContext
+{
+    public DbSet<Person1> People1 { get; set; }
+    public DbSet<Person2> People2 { get; set; }
+    public DbSet<Person3> People3 { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // UseSqlServer methodu => Database Provider 
+        optionsBuilder.UseSqlServer("Server=localhost;Database=backing_field_db;User Id=mucahid;Password=test12345;TrustServerCertificate=True;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Person3>()
+            .Property(p => p.Name)
+            .HasField(nameof(Person3.name))
+            .UsePropertyAccessMode(PropertyAccessMode.Property);
+        //PropertyAccessMode.Field
+
+    }
+}
--- DatabaseFirst/obj/Debug/net8.0/DatabaseFirst.AssemblyInfo.cs
+++ DatabaseFirst/obj/Debug/net8.0/DatabaseFirst.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DatabaseFirst")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a63236ec0376fd5207018860b949d7fb2daf7ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dd1786b6af7ecd8aa6a6606349c1a786b7b911a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DatabaseFirst")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DatabaseFirst")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
